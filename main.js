4167007d651ac6c5b0f0762708bc4c6cc4e9eae1
DongGyu <ehdrb700606@naver.com>
2022-05-21 15:39:45 +0900

    project connect DB(MySQL), write CRUD USING MySQL query

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+const mysql = require('mysql')
+const Connection = require('mysql/lib/Connection')
 const app = express()
 const port = 8080
+
+const con= mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'user'
+})
+
+con.connect((err)=>{
+  if(err) throw err;
+  console.log('Connected DB')
+})
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,12 +29,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users',(req,res)=>{
-  res.render(__dirname + '/users.ejs',{data}) // json 을 전송하여 ejs 파일에서 변수 출력할 수 있도록 함
+  con.query("SELECT * FROM USERS",(err,results,fields)=>{
+    if(err)throw err;
+
+    res.render(__dirname + '/users.ejs',{results}) // json 을 전송하여 ejs 파일에서 변수 출력할 수 있도록 함
+  })
 })
 
 app.post('/users/create',(req,res)=>{
-  data.push(req.body); 
-  console.log(data)
+  con.query("INSERT INTO USERS VALUES(?,?,?)",
+    [req.body.id,req.body.pw,req.body.name],
+    (err,results,fields)=>{
+    if(err) throw err;
+    console.log("INSERT COMPLETE")
+  })
+
   res.redirect('/users')
 })
 
@@ -29,26 +52,31 @@ app.get('/users/create',(req,res)=>{
 })
 
 app.get('/users/:userid',(req,res)=>{
-  console.log('route')
-  var Rdata = null
-  for( let i of data){
-    console.log(typeof(i.id) + " " + typeof(req.params.userid))
-    if(i.id == req.params.userid){ //파라미터를 사용
-      Rdata = i;
+
+  con.query("SELECT * FROM USERS WHERE userid=?",[req.params.userid],(err,results,fields)=>{
+    if(err)throw err;
+
+    console.log('db접근')
+    console.log(results);
+    if(results.length == 0){
+      res.send('해당 사용자를 찾을 수 없습니다.')
     }
-  }
-  if(Rdata == null)res.send('사용자를 찾을 수 없습니다.')
-  else{res.render(__dirname + '/user.ejs',{Rdata})}
+    else{
+      res.render(__dirname + '/user.ejs',{results})
+    }
+    
+  })
+
 })
 
 app.post('/users/:userid/delete',(req,res)=>{
-  console.log('parse')
-  for(let i in data){
-    if(data[i].id == req.params.userid){
-      data.splice(i,1);
-    }
-  }
-  res.redirect('/users')
+  
+  con.query('DELETE FROM USERS WHERE userid=?',[req.params.userid],(err,results,field)=>{
+    if(err) throw err;
+    console.log('DELETE COMPLETE')
+    res.redirect('/users')
+  })
+  
 })
 
 
